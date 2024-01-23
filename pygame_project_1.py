408747b16bb2a9233d40eab65dfb8362d23d8f16
RyanFields <rienfeels14@gmail.com>
2024-01-23 09:04:31 -0500

    pygame commit

--- pygame_project_1.py
+++ pygame_project_1.py
@@ -9,6 +9,7 @@ game_folder = os.path.dirname(__file__)
 img_folder = os.path.join(game_folder, 'img')
 player_img = pygame.image.load(os.path.join('p1_jump.png'))
 
+
 # Initialize pygame and create the window
 pygame.mixer.init()
 screen = pygame.display.set_mode((460, 580))  # Set an initial size for the display
@@ -19,10 +20,13 @@ all_sprites = pygame.sprite.Group()
 # Convert the player_img surface after initializing the display
 player_img = pygame.Surface.convert(player_img)
 
+BLACK = (0, 0, 0)
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = player_img
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
         self.rect.center = (460 / 2, 580 / 2)
 
